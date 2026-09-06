54543935bdffc72ec534391a31c203bd06a7b3d9
vidunisalitha <vidunisalitha@gmail.com>
2026-09-06 10:16:50 +0530

    Initial commit

---
+++ backend/src/server.js
@@ -0,0 +1,16 @@
+import express from "express";
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.json({
+        message: "Express server is running"
+    });
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});
